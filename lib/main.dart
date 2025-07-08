72be9a7e5268c97c8f6f559e5c3200975b1545bf
fredyallard01 <fredy.allard@utp.ac.pa>
2025-07-08 04:25:35 -0500

    Cambios en

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/movie_provider.dart';
+import 'providers/movie_provider.dart';
 import 'screens/home_screen.dart';
 
 void main() {
